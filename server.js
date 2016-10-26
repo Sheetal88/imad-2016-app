11216ef7439f641dd1351c59f5191d9c6d76fd12
Sheetal88 <sheetaljagtap88@gmail.com>
2016-10-26 13:19:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleOne', function(req, res) {
+    res.send('Article one requested and will be served here');
+});
+
+app.get('/articleTwo', function(req, res) {
+    res.send('Article two requested and will be served here');
+});
+
+app.get('/articleThree', function(req, res) {
+    res.send('Article three requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

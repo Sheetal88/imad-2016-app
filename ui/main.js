d41217d8bdeb41584d22f1853495c341bb229ba6
Sheetal88 <sheetaljagtap88@gmail.com>
2016-10-30 15:53:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+//change the text of main-text div
+
+var element = document.getElementById('main-text');
+element.innerHTML = 'This is my webpage';
+
+//make an element move right
+
+var img = document.getElementById('madi');
+
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
+
+    

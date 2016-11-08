edd714a4d533a4be345ea012ad5ebfcf40dc2b05
Sheetal88 <sheetaljagtap88@gmail.com>
2016-11-08 14:21:39 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,17 @@
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://sheetal88.imad.hasura-app.io/counter',true);
+    request.send(null);
+};

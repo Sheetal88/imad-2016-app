65999def56fa6cd8c68a4f780046534e5dce1365
Sheetal88 <sheetaljagtap88@gmail.com>
2016-11-08 14:09:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,15 @@ app.get('/counter', function(req, res){
    res.send(counter.toString());
 });
 
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function() {
+    counter = counter + 1;
+    var apan = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);

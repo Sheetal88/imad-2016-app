020ef98489bc37ef28eb975f111cb3df70b2d9b1
Sheetal88 <sheetaljagtap88@gmail.com>
2016-11-08 14:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById('counter');
-var counter = 0;
-
-button.onclick = function() {
-    counter = counter + 1;
-    var apan = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};

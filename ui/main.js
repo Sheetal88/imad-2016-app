1af419aea57b43e838b51fa24988be688c04e373
Sheetal88 <sheetaljagtap88@gmail.com>
2016-11-08 17:53:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,24 @@ button.onclick = function() {
     request.open('GET','http://sheetal88.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if(request.status === 200)
+        {
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i=0; i<names.length; i++){
+                list += '<li>' + name[i] + '</li>';
+            }
+            var ul=document.getElementById('names.length; i++');
+            ul.innerHTML = list;
+        }
+    }
+    var nameInput = document.getElementById('name');
+    var name = nameinput.value;
+    request.open('GET','http://sheetal88.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
